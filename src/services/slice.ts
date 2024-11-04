e12eeba34123e716ea22c4d2f7965f0eb1bcf633
ShivdeepChaudhari23 <shivdeepchaudhari23@gmail.com>
2024-11-04 11:31:13 +0530

    Environment and Build Setup

--- src/services/slice.ts
+++ src/services/slice.ts
@@ -1,10 +1,10 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-
+const baseUrl = import.meta.env.VITE_API_URL;
 const treeSlice = createApi({
     reducerPath: 'treeSlice',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'http://localhost:3000',
+        baseUrl,
     }),
     endpoints: () => ({}),
     tagTypes: [
